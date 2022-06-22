5a46b844a72751ac5d8030e1d50f5aaf7f089523
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-06-22 15:44:44 +0530

    'Create Customer CRUD operations'

--- BackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,11 +1,48 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("customer")
 @CrossOrigin
 public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCustomer(){
+        return new ResponseUtil(200,"Successfully Loaded",customerService.getAllCustomer());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO customer){
+        customerService.saveCustomer(customer);
+        return new ResponseUtil(200,"Saved",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCustomer(@RequestParam String id) {
+        customerService.deleteCustomer(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO customer){
+        customerService.updateCustomer(customer);
+        return new ResponseUtil(200,"Updated",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCustomer(@PathVariable String id) {
+        CustomerDTO customerDTO = customerService.searchCustomer(id);
+        return new ResponseUtil(200,"Found",customerDTO);
+    }
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,9 +1,11 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,22 +23,42 @@ public class CustomerServiceImpl implements CustomerService {
     private ModelMapper modelMapper;
 
     public void saveCustomer(CustomerDTO dto) {
-
+        if (!customerRepo.existsById(dto.getId())) {
+            Customer map = modelMapper.map(dto, Customer.class);
+            customerRepo.save(map);
+        } else {
+            throw new RuntimeException("Customer Already Exist..!");
+        }
     }
 
     public void deleteCustomer(String id) {
-
+        if (customerRepo.existsById(id)){
+            customerRepo.deleteById(id);
+        }else{
+            throw new RuntimeException("Please check the Customer ID.. No Such Customer..!");
+        }
     }
 
     public void updateCustomer(CustomerDTO dto) {
-
+        if (customerRepo.existsById(dto.getId())){
+            Customer map = modelMapper.map(dto, Customer.class);
+            customerRepo.save(map);
+        }else {
+            throw new RuntimeException("No Such a Customer..!");
+        }
     }
 
     public CustomerDTO searchCustomer(String id) {
-        return null;
+        if (customerRepo.existsById(id)){
+            Customer customer = customerRepo.findById(id).get();
+            return modelMapper.map(customer, CustomerDTO.class);
+        }else{
+            throw new RuntimeException("No Customer For "+id+" ..!");
+        }
     }
 
     public List<CustomerDTO> getAllCustomer() {
-        return null;
+        return modelMapper.map(customerRepo.findAll(), new TypeToken<List<CustomerDTO>>() {
+        }.getType());
     }
 }
